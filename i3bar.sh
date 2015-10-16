0da6bf496d59283938546c3e009f1dc95709ee60
Tom Hardy <wuxmedia@gmail.com>
2015-10-16 11:54:34 +0100

    Create i3bar.sh
    this is for i3's bar, otherwise just put the wget where ever  your bar like it

---
+++ i3bar.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# shell script to prepend i3status with more stuff
+
+i3status | while :
+ do
+  read line
+  echo "$(wget -q -O - http://domain.com/random.txt && mocp -Q %title ) | $line" || exit 1
+ done
